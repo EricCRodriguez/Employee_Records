9f652631aa0c658bba82e828032328fdb3fc38b0
EricCRodriguez <ecrod12@gmail.com>
2022-09-19 23:08:43 -0500

    setup routes.rb/CRUD API

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+   namespace :api do
+    get "/records" => "records#index"
+    post "/records" => "records#create"
+    get "/records/:id" => "records#show"
+    patch "/records/:id" => "records#update"
+    delete "/records/:id" => "records#destroy"
+  end
+end
 end
